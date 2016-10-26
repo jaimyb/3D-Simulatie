20b16982849f45e274f7075a681778b3ccfc226c
JaimyGanja420 <jaimybeima@live.nl>
2016-10-26 16:40:05 +0200

    Begin van fps controls

---
+++ Jaimy/Assets/Camera Movement.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+//Variables
+var rotateSpeed = 50f;
+
+
+
+
+
+function cameraAxisMovement () {
+
+	var rotateVector = new Vector3(0f,0f,0f);
+
+	if(Input.GetAxis("Mouse X") < 0)
+		rotateVector.y += -1;
+
+	if(Input.GetAxis("Mouse X") > 0)
+		rotateVector.y += 1;
+
+	if(Input.GetAxis("Mouse Y") < 0)
+		rotateVector.x += -1;
+
+	if(Input.GetAxis("Mouse Y") > 0)
+		rotateVector.x += 1;
+
+	transform.Rotate(rotateVector, rotateSpeed * Time.deltaTime);
+		
+}
+
+
+function Update () {
+	cameraAxisMovement();
+}
