e096680e699ea721089e1b426410e5e354af1e29
JaimyGanja420 <jaimybeima@live.nl>
2016-10-28 11:52:28 +0200

    This time drizzle is enough for my bizzle

---
+++ Jaimy/Assets/Camera Movement.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+
+//Variables
+
+
+var lockPos = 0;
+
+//Options
+Screen.lockCursor = true;
+
+
+
+//Functions
+function cameraAxisRotate () {
+	var rotateSpeed = 200f;
+	var rotateVector = new Vector3(0f,0f,0f);
+
+	if(Input.GetAxis("Mouse X") < 0)
+		rotateVector.y += -1;
+
+	if(Input.GetAxis("Mouse X") > 0)
+		rotateVector.y += 1;
+
+	if(Input.GetAxis("Mouse Y") < 0)
+		rotateVector.x += 1;
+
+	if(Input.GetAxis("Mouse Y") > 0)
+		rotateVector.x += -1;
+
+	transform.Rotate(rotateVector, rotateSpeed * Time.deltaTime);
+		
+}
+
+function cameraAxisMovement () {
+	var sensetivity = 0.01f;
+	var movementVector = new Vector3(0f,0f,0f);
+	var movementSpeed = 0.1f;
+	var hMove = Input.GetAxis("Horizontal");
+	var vMove = Input.GetAxis("Vertical");
+
+	movementVector.y = 0f;
+
+	if(hMove < -sensetivity)
+		movementVector.x += -movementSpeed;
+	if(hMove > sensetivity)
+		movementVector.x += movementSpeed;
+	if(vMove < -sensetivity)
+		movementVector.z += -movementSpeed;
+	if(vMove > sensetivity)
+		movementVector.z += movementSpeed;
+
+	
+
+	transform.Translate(movementVector);
+}
+
+function Update () {
+    cameraAxisRotate();
+    cameraAxisMovement();
+    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, lockPos);
+    transform.position.y = 3;
+}
---
+++ Jaimy/Assets/CapsuleMovement.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+//var sensetivity = 0.1;
+
+var minimumY = -60;
+var maximumY = 60;
+
+var rotationY = 0;
+var rotationX = 0;
+
+function Update () {
+	var sensetivity = 0.1;
+	rotationX = transform.localEulerAngles.y + Input.GetAxis("MouseX") * sensetivity;
+
+	rotationY += Input.GetAxis("MouseY") * sensetivity;
+	rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+	transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+	while(Input.GetKeyDown("w")){
+		
+		transform.position.z += 1;
+	}
+		
+}
