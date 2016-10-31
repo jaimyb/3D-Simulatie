4c204657f5e5e1de9ac1a56c1b1982abc84512f5
WJZwager <wijbe.zwager@hotmail.com>
2016-10-31 16:27:57 +0100

    update collision

---
+++ Maze 2.0/Assets/trigger_walls.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+
+function OnTriggerEnter(theCollision : Collider){
+	Debug.Log(theCollision.name);
+	if(theCollision.gameObject.name == "triggerCube"){
+		Debug.Log("triggered");
+	}
+}
