9f723a9f2666d3731e52eba2e427c9ad024e4a18
Jaimy <jaimybeima@live.nl>
2016-11-02 11:55:48 +0100

    Divin' deep

--- Maze 2.0/Assets/PowerUpWallColor.js
+++ Maze 2.0/Assets/PowerUpWallColor.js
@@ -2,14 +2,13 @@
 var door : GameObject;
 var triggered = false;
 
-door = GameObject.Find("Cube (38)");
+door = GameObject.Find("Cube (32)");
 
 function OnTriggerEnter(theCollision : Collider)
 {
 	if(theCollision.gameObject.tag == "Player")
 	{
-			door.GetComponent<Renderer>().material.color = Color.red;
-			Debug.Log("Nice");
+			door.GetComponent.<Renderer>().material.color = Color.red;
 			triggered = true;
 	}
 }
