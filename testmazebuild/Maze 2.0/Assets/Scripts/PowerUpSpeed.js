c5a0cb592d759c400278f6298c4a5711aa7b91b7
JaimyGanja420 <jaimybeima@live.nl>
2016-11-04 13:58:40 +0100

    Beginning of powerup spawn points.

---
+++ testmazebuild/Maze 2.0/Assets/Scripts/PowerUpSpeed.js
@@ -0,0 +1,34 @@
+﻿var triggered = false;
+var time: float;
+var speler: GameObject;
+
+speler = GameObject.Find("Capsule");
+
+function OnTriggerEnter(theCollision : Collider)
+{
+	if(theCollision.gameObject.tag == "Player")
+	{		
+			if(!triggered)
+				speler.GetComponent(PlayerMovementScript).SpeedBoost();
+				//door.GetComponent.<Renderer>().material.color = Color.blue;
+
+			triggered = true;
+			GetComponent(MeshRenderer).enabled = false;
+
+			if(triggered)
+			{
+				yield WaitForSeconds(10);
+				//door.GetComponent.<Renderer>().material.color = lastColor;
+				gameObject.SetActive(false);
+			}
+
+
+	}
+}
+
+
+function Update () 
+{
+	time += Time.deltaTime * 3;
+	transform.localPosition.y = 0.5 * Mathf.Cos(time) + 1.5;
+}
--- testmazebuild/Maze 2.0/Assets/Scripts/PowerUpWallColor.js
+++ testmazebuild/Maze 2.0/Assets/Scripts/PowerUpWallColor.js
@@ -1,22 +1,38 @@
 ﻿
-/*var door : GameObject;
+var door : GameObject;
 var triggered = false;
+var time: float;
+var lastColor = new Color32(236, 63, 203, 1);
 
-door = GameObject.Find("Cube (38)");
+//door = GameObject.Find("Cube (32)");
 
 function OnTriggerEnter(theCollision : Collider)
 {
 	if(theCollision.gameObject.tag == "Player")
-	{
-			door.GetComponent<Renderer>().material.color = Color.red;
-			Debug.Log("Nice");
+	{		
+			if(!triggered)
+				//door.GetComponent.<Renderer>().material.color = Color.blue;
+
 			triggered = true;
+			GetComponent(MeshRenderer).enabled = false;
+	
+			GetComponent.<AudioSource>().Play();
+
+			if(triggered)
+			{
+				yield WaitForSeconds(10);
+				//door.GetComponent.<Renderer>().material.color = lastColor;
+				gameObject.SetActive(false);
+			}
+
+
 	}
 }
 
+function Update () 
+{
+	time += Time.deltaTime * 3;
+	transform.localPosition.y = 0.5 * Mathf.Cos(time) + 1.5;
 
 
-function Update () {
-	
-
-}*/
+}
