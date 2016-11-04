39f491630f0c23c1106e65540417f5f9756fc415
RSlomp <slom1500@student.nhl.nl>
2016-11-04 13:24:29 +0100

    New Maze

---
+++ Maze 2.0/Assets/Scripts/TimerScript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+import UnityEngine.UI;
+static var time : int;
+static var finishtime : int;
+static var trigger : boolean;
+private var text : Text;
+
+function Awake ()
+{
+	text = GetComponent (Text);
+}
+
+function Update (){
+time = Time.realtimeSinceStartup;
+if (trigger == false){
+text.text = "time: "+ time;
+}
+if (trigger == true){
+text.text = "time: " + finishtime;
+	}
+}
+
+function stop(){
+finishtime = time;
+trigger = true;
+}
